4f92b9a796d11bb5e3224bd95199b16193f05064
YuanLiSDU <lyuan@mail.sdu.edu.cn>
2026-06-20 07:56:51 +0800

    use 2nd order non-linearity correction

--- prad2det/src/HyCalCluster.cpp
+++ prad2det/src/HyCalCluster.cpp
@@ -439,11 +439,15 @@ ClusterHit HyCalCluster::reconstruct_pos(const ModuleCluster &cl) const
     result.flag      = cl.flag;
     result.linear_corr = 1.f;
 
-    //non-linearity correction: E + nl * (E - E_cal) / 1000
-    float cal_non_linear = sys_.GetCalibNonLinearity(center_mod.id);
+    //non-linearity correction: E + nl1 * (E - E_cal) / 1000 + nl2 * (E - E_cal)^2 / 1000^2
+    float cal_non_linear = sys_.GetCalibNonLinearity1(center_mod.id);
     float cal_base_energy = sys_.GetCalibBaseEnergy(center_mod.id);
     if (cal_non_linear != 0.f && cl.energy > 0.f) {
+        float cal_non_linear2 = sys_.GetCalibNonLinearity2(center_mod.id);
         result.energy = cl.energy + cal_non_linear * (cl.energy - cal_base_energy) / 1000.f;
+        if (cal_non_linear2 != 0.f) {
+            result.energy += cal_non_linear2 * (cl.energy - cal_base_energy) * (cl.energy - cal_base_energy) / (1000.f * 1000.f);
+        }
         result.linear_corr = result.energy / cl.energy;
     }
 
